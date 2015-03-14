9cce12b58ee47cda8ebb4f79b4978d02ddf33648
Betobit <jesusmartinoza@gmail.com>
2015-03-14 14:21:39 -0600

    Nuevo diseño en Inicio, menos peticiones. En /infocomp documentos cargados

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -15,7 +15,7 @@ angular.module('infocomp.controllers', [])
 .controller('InicioCtrl', function($scope, $http) {
 	$scope.titulo = "Noticias",
 	$scope.noticias = []
-	$http.get('http://graph.facebook.com/acominf/photos/uploaded?fields=source,name')
+	$http.get('http://graph.facebook.com/acominf/photos/uploaded?fields=source,name,link&limit=13')
 		.success(function(response) {
 
 			var noticias = response.data;
@@ -26,6 +26,7 @@ angular.module('infocomp.controllers', [])
 					$scope.noticias.push(noticias[i])
 				}
 			}
+			console.log(response.data)
 
 		}).error(function(error) {
 			console.log(error)
@@ -35,7 +36,18 @@ angular.module('infocomp.controllers', [])
 
 
 .controller('CarrerasCtrl', function($scope, $routeParams) {
-	$scope.titulo = 'Ingeniería en '+$routeParams.carrera
+	$scope.titulo = 'Ingeniería en '
+	$scope.imagenGrande = 'css/img/carreras' 
+
+	if($routeParams.carrera == 'computacion')
+	{		
+		$scope.titulo += 'Computación'
+		$scope.imagenGrande += '/computacion.jpg' 
+	}else{
+		$scope.titulo += 'Informática'
+		$scope.imagenGrande += '/informatica.jpg' 
+	}
+
 })
 
 .controller('ProyectosCtrl', function($scope) {
@@ -59,4 +71,57 @@ angular.module('infocomp.controllers', [])
 
 .controller('InfocompCtrl', function($scope) {
 	$scope.titulo = 'INFOCOMP'
+	$scope.documentosNormativa = [
+		{
+			nombre : 'Estatuto Orgánico',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/estatuto.pdf'
+		},
+		{
+			nombre : 'Reglamento Personal Académico',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/reglamento_del%20personal_academico.pdf'
+		},
+		{
+			nombre : 'Reglamento de Examenes',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/examen.pdf'
+		},
+		{
+			nombre : 'Reglamento de Diplomados',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/diplomados.pdf'
+		},
+		{
+			nombre : 'Reglamento de Posgrados',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/posgrado.pdf'
+		},
+		{
+			nombre : 'Reglamento de Permisos y Licencias',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/permisos.pdf'
+		}
+	]
+
+	$scope.documentosFacultad = [
+		{
+			nombre : 'Manual de Organización',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/manorg.pdf'
+		},
+		{
+			nombre : 'Reglamento Interno',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/regint.pdf'
+		},
+		{
+			nombre : 'Reglamento interno',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/procedi.pdf'
+		},
+		{
+			nombre : 'Manual de Procedimientos',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/diplomados.pdf'
+		},
+		{
+			nombre : 'Desarrollo Curricular y Academias',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/descurr.pdf'
+		},
+		{
+			nombre : 'Movilidad Académicas Estudiantil',
+			link : 'http://infocomp.ingenieria.uaslp.mx/infocomp/docs/normativa/movacad.pdf'
+		}
+	]
 })
